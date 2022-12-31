4e3451617560f18ade6e38982d899a68c515b021
brianttn <brianttn@gmail.com>
2022-12-31 19:00:19 +0800

    Add userRouteAPI.js CRUD operations

---
+++ src/routes/userRouteAPI.js
@@ -0,0 +1,10 @@
+/* ~ ~ ~ ~ ~ ~ ~ ~ ~     routes/routeUser.js     ~ ~ ~ ~ ~ ~ ~ ~ ~ */
+const bcrypt = require("bcrypt");
+/* - - - - - 引入 => 「Model：userConstructor」 - - - - - */
+const userConstructor = require("../models/User");
+
+const userRouteAPI = (app, cors) => {
+
+}
+
+module.exports = userRouteAPI
