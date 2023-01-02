16e7a96ed2bf1af433f2ffb1612353015c9df919
brianttn <brianttn@gmail.com>
2023-01-03 01:08:15 +0800

    Revise MongoDB URL & Pasword

--- src/db/mongooseConnectMongoDB.js
+++ src/db/mongooseConnectMongoDB.js
@@ -1,16 +1,14 @@
-/* ~ ~ ~ ~ ~ ~ ~ ~ ~   src/db/mongooseConnectMongoDB.js   ~ ~ ~ ~ ~ ~ ~ ~ ~ */
+* ~ ~ ~ ~ ~ ~ ~ ~ ~   src/db/mongooseConnectMongoDB.js   ~ ~ ~ ~ ~ ~ ~ ~ ~ */
 const mongoose = require('mongoose')
 
-/* = = = = = = = =   連線到MongoDB的「database：jwtTest」   = = = = = = = = */
-const mongodbPwd = process.env.MONGODB_PASSWORD
-const dataBase = 'jwtTest';
+/* = = = = = = = =   連線到Railway上的MongoDB：無法設定「預設Database」   = = = = = = = = */
+const mongodbPwd = process.env.MONGODB_PASSWORD;
 
 /*--- 使用IIFE時，記得「上一個：statement」的「結尾」一定要加「;」 ---*/
 (
     async () => {
         try {
-            await mongoose.connect(`mongodb+srv://brianttn:${mongodbPwd}@cluster0.yexhqjq.mongodb.net/${dataBase}?retryWrites=true&w=majority`)
-            console.log(`Database：${dataBase} is connected!!`)
+            await mongoose.connect(`mongodb://mongo:${mongodbPwd}@containers-us-west-17.railway.app:6458`)
         } catch (err) {
             console.error("Database connection error!!", err)
         }
